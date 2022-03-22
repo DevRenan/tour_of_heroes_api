0a2e7ab9701d1230e9aed6f88c83c69dce8f1af1
Renan Cruz <rcruz.vermont@alelo.com.br>
2022-03-22 14:36:53 -0300

    Adiciona busca na lista de heróis

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -2,4 +2,5 @@ class Hero < ApplicationRecord
   validates :name, presence: true
 
   scope :sorted_by_name, -> {order(:name)}
+  scope :search, ->(term) { where("LOWER(name) like ?", "%#{term.downcase}%") if term.present?}
 end
